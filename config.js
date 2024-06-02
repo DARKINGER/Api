9ad2a5014788fb7e211f5f84f90765dee2ec980c
DARKINGER <alexanfmdm@gmail.com>
2024-06-01 19:44:49 -0500

    prueba 5

--- config.js
+++ config.js
@@ -1,1 +1,16 @@
+// import { Config } from 'dotenv'
+require('dotenv').config()
 
+// config()
+
+const port = process.env.DB_port || 3036
+const PORT = process.env.DB_PORT || '3000'
+const HOST = process.env.DB_HOST || 'localhost'
+const USER = process.env.DB_USER || 'admin'
+const PASSWORD = process.env.DB_PASSWORD || 'Dima.zdla1'
+const DATABASE = process.env.DB_DATABASE || 'sputyfy'
+// export const HOST = process.env.HOST || 'localhost'
+
+module.exports = {
+    PORT, HOST, USER, PASSWORD, DATABASE, port
+}
